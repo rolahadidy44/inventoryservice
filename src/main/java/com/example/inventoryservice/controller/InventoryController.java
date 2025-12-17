b87d956825cad9289b988d204b439d912af5290c
Rola Hadidy <rolatotta2004@gmail.com>
2025-12-17 19:44:00 +0200

    first commit

---
+++ src/main/java/com/example/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,29 @@
+package com.example.inventoryservice.controller;
+
+
+import com.example.inventoryservice.response.EventInventoryResponse;
+import com.example.inventoryservice.service.InventoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1")
+public class InventoryController {
+
+    private InventoryService inventoryService;
+    @Autowired
+    public InventoryController(final InventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+    @GetMapping("/inventory/events")
+    public @ResponseBody List<EventInventoryResponse> inventoryGetAllEvents(){
+        return inventoryService.getAllEvents();
+    }
+
+}
---
+++ src/main/java/com/example/inventoryservice/response/EventInventoryResponse.java
@@ -0,0 +1,4 @@
+package com.example.inventoryservice.response;
+
+public class EventInventoryResponse {
+}
